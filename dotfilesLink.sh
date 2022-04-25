e5d73bb6685d5e1a9806d8782e797aa3d0a51638
ckp-koiken <kentaro.koi0710@gmail.com>
2022-04-25 12:22:15 +0900

    switching platform

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,8 +1,17 @@
-#! /bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.latexmkrc ~/.latexmkrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.screenrc ~/.screenrc
-ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
-
+#!/bin/sh
+if [ "$(uname)" == 'Darwin' ]; then
+  ln -sf ~/dotfiles/.vimrc ~/.vimrc
+  ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+  ln -sf ~/dotfiles/.screenrc ~/.screenrc
+  ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
+  ln -sf ~/dotfiles/.zshrc ~/.zshrc
+  ln -sf ~/dotfiles/.latexmkrc ~/.latexmkrc
+elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
+  ln -sf ~/dotfiles/.vimrc ~/.vimrc
+  ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+  ln -sf ~/dotfiles/.screenrc ~/.screenrc
+  ln -sf ~/dotfiles/.emacs.d ~/.emacs.d
+else
+  echo "Your platform ($(uname -a)) is not supported."
+  exit 1
+fi
